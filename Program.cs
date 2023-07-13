6875a801f47967a744cb90154750b52f3343c7cd
malviyaashish <78544212+malviyaashish@users.noreply.github.com>
2023-07-13 13:24:26 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            DateTime dat = DateTime.Now;
+
+            Console.WriteLine("Today is {0:d} at {0:T}.", dat);
+            Console.WriteLine("Version: 1.0.0");
+            Console.WriteLine("CPP Demo");
+
+            //Console.Write("\nPress any key to continue... ");
+            //Console.ReadLine();
+        }
+    }
+}
